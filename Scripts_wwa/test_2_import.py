29c281782ecae48927949d8fab0bb9669b4c7c01
Grasjusz <46102483+Grasjusz@users.noreply.github.com>
2025-03-27 14:56:38 +0100

    Edit

--- Scripts_wwa/test_2_import.py
+++ Scripts_wwa/test_2_import.py
@@ -1,13 +1,13 @@
 import jarray
 import jmri
-import sys
-# Dodaj ścieżkę do katalogu, w którym znajduje się biblioteka Kollib.py
-sys.path.append(r'C:\Users\LOK_7\JMRI\My_JMRI_Railroad.jmri\MyScrypt')
+import sys, os
+#Pobiera relatywną ścieżkę do skryptu (czyli przeszukuje katalog główny w ktorym jest uruchamiany skrypt)
+sys.path.append(os.path.join(sys.path[0]))
 import Kollib #Biblioteka autorskich funkcji
 
 #Sekwencyjne przypisywanie adresów sensorą 
-FirstSensorAdress = 1
-NumberOfSensors = 32
+FirstSensorAdress = 9 #Wpisz numer pierwszego czujnika danej sekcji
+NumberOfSensors = 25 #wpisz ilość czujników (-1 pierwszy wpisany wyżej)
 SensorsList = []
 for i in range(FirstSensorAdress, FirstSensorAdress + NumberOfSensors):
     SensorsList.append(sensors.getSensor("LS"+str(i-1)))
@@ -27,40 +27,33 @@ odnoszaca sie do tego czujnika.
 '''
 
 
-class kolejka_test(jmri.jmrit.automat.AbstractAutomaton) :
+def init(self):
+    # init() is called exactly once at the beginning to do
+    # any necessary configuration.
+    print ("Inside init(self)")
+    # get loco address. For long address change "False" to "True"
+    self.throttle = self.getThrottle(3, False)  # short address 14
+    return
 
-    def init(self):
-        # init() is called exactly once at the beginning to do
-        # any necessary configuration.
-        print "Inside init(self)"
-
-        # get loco address. For long address change "False" to "True"
-        self.throttle = self.getThrottle(3, False)  # short address 14
-        return
-
-    def handle(self):
-        # handle() is called repeatedly until it returns false.
-        print "Inside handle(self)"
-        self.throttle.setSpeedSetting(0)# Upewnia sie że kolejka jest zatszymana
-        self.throttle.setF0(True)
-        self.throttle.setF1(True)
-        Kollib.drive_vehicle(self, 0.3, True)
-        self.waitMsec(1000)
-        Kollib.sc_station(self, SensorsList[28], 2)
-        self.waitMsec(1000)
-        Kollib.sc_station(self, SensorsList[27], 0.5)
-        Kollib.stop_at_station(self, SensorsList[26], 5000)
-        Kollib.drive_vehicle(self, 0.3, False)
-        Kollib.sc_station(self, SensorsList[27], 1.5)
-        Kollib.stop_at_station(self,SensorsList[29], 1000)
-        Kollib.drive_vehicle(self, 0.4, True)
-        Kollib.delay_stop(self, SensorsList[28],20000)
-        Kollib.drive_vehicle(self, 0.3, False)
-        Kollib.stop_at_station(self, SensorsList[29], 5000)
-        self.throttle.setF1(False)
-        print "End of Loop"
-        return 1
-      
-
-
-kolejka_test().start()
+def handle(self):
+    # handle() is called repeatedly until it returns false.
+    print ("Inside handle(self)")
+    self.throttle.setSpeedSetting(0)# Upewnia sie że kolejka jest zatszymana
+    self.throttle.setF0(True)
+    self.throttle.setF1(True)
+    Kollib.drive_vehicle(self, 0.3, True)
+    self.waitMsec(1000)
+    Kollib.sc_station(self, SensorsList[28], 2)
+    self.waitMsec(1000)
+    Kollib.sc_station(self, SensorsList[27], 0.5)
+    Kollib.stop_at_station(self, SensorsList[26], 5000)
+    Kollib.drive_vehicle(self, 0.3, False)
+    Kollib.sc_station(self, SensorsList[27], 1.5)
+    Kollib.stop_at_station(self,SensorsList[29], 1000)
+    Kollib.drive_vehicle(self, 0.4, True)
+    Kollib.delay_stop(self, SensorsList[28],20000)
+    Kollib.drive_vehicle(self, 0.3, False)
+    Kollib.stop_at_station(self, SensorsList[29], 5000)
+    self.throttle.setF1(False)
+    print ("End of Loop")
+    return 1
